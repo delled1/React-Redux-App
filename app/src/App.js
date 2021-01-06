b582e0db232da3d5dd9742ab637acbd20dd40877
Eric Della <ericdella13@gmail.com>
2021-01-05 16:09:05 -0800

    added action, components, reducers folders & setup food component

---
+++ app/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Food from "./components/food"
+
+function App() {
+  return (
+    <div className="App">
+      <Food />
+    </div>
+  );
+}
+
+export default App;
---
+++ app/src/components/food.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Food =  () => {
+    return (
+        <div>
+        <h1>FOOD</h1>
+        <button>Get New Image</button>
+        </div>
+    )
+}
+
+export default Food
